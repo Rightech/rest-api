45627786be95df6455f737862d1fef5e37cce588
Oleg Prohazko <olegprohazko@gmail.com>
2021-03-20 01:08:23 +0300

    lib - resolve default client

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const { CODES, ApiError, NginxError, Client } = require("./dist/api");
+const { default: _default, ...rest } = require("./dist/api");
 
-function getClient({ url, token } = {}) {
-  const baseUrl =
-    url ||
-    process.env["RIC_BASE_URL"] ||
-    process.env["RIC_URL"] ||
-    "https://dev.rightech.io/";
-
-  return new Client({
-    url: baseUrl,
-    token: token || process.env["RIC_TOKEN"],
-  });
-}
-
-const defaultClient = getClient();
-
-Object.assign(defaultClient, {
-  getClient,
-  CODES,
-  ApiError,
-  NginxError,
-  Client,
-});
-
-module.exports = defaultClient;
+module.exports = Object.assign(_default, rest);
